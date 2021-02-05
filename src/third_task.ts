8ce14ee2f91e5b018a862e964408be3e4f2e1925
Aidys Dongak <aidongak23@gmail.com>
2021-02-05 11:47:24 +0700

    add third task

---
+++ src/third_task.ts
@@ -0,0 +1,9 @@
+type FIXME = any;
+
+export const omit = <T extends Record<any, any>, K extends keyof T>(
+  obj: T,
+  keyToOmit: K
+): FIXME => {
+  const { [keyToOmit]: _, ...withoutKey } = obj;
+  return withoutKey;
+};
