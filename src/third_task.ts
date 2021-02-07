193cb14561af2af3806565f3cf3a05493a6424ee
aidys23 <aidongak23@gmail.com>
2021-02-07 19:29:33 +0700

    change utility type

--- src/third_task.ts
+++ src/third_task.ts
@@ -1,7 +1,7 @@
 export const omit = <T extends Record<any, any>, K extends keyof T>(
   obj: T,
   keyToOmit: K
-): Pick<T, Exclude<keyof T, K>> => {
+): Omit<T, K> => {
   const { [keyToOmit]: _, ...withoutKey } = obj;
   return withoutKey;
 };
